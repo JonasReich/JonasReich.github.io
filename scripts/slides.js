2eeb56a2932d5f03c475ee2862658485ebba792e
Jonas Reich <joreich@stud.mediadesign.de>
2017-02-08 01:25:06 +0100

    working carousel

--- scripts/slides.js
+++ scripts/slides.js
@@ -1,27 +1,37 @@
 
 var slideIndex = 1;
+var carouselDelay = 5; // in seconds
+
+//Uncomment to enable carousel:
+//carousel();
+
 showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+	showDivs(slideIndex += n);
 }
 
 function currentDiv(n) {
-  showDivs(slideIndex = n);
+	showDivs(slideIndex = n);
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("slideDots");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" slideDots-selected", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " slideDots-selected";
+	var i;
+	var x = document.getElementsByClassName("slides");
+	var dots = document.getElementsByClassName("slideDots");
+	if (n > x.length) { slideIndex = 1 }
+	if (n < 1) { slideIndex = x.length }
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" slideDots-selected", "");
+	}
+	x[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " slideDots-selected";
+}
+
+function carousel() {
+	plusDivs(+1);
+	setTimeout(carousel, carouselDelay * 1000);
 }
