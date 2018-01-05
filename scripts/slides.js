9e16e47c95b237da64e155660bc8f2b36a22bf99
Jonas Reich <mail@jonasreich.de>
2018-01-05 02:05:42 +0100

    Greatly improve slideshows

--- scripts/slides.js
+++ scripts/slides.js
@@ -1,50 +1,76 @@
 
-var slideIndex = 1;
-var carouselDelay = 5; // in seconds
+function showSlide(targetId, slideIndex) {
+	const slideContainer = document.getElementById(targetId);
+	const slides = slideContainer.children;
 
-//Uncomment to enable carousel:
-//carousel();
+	while (slideIndex >= slides.length) { slideIndex -= slides.length; }
+	while (slideIndex < 0) { slideIndex += slides.length; }
 
-//showDivs(slideIndex, "slides");
 
-function plusDivs(n, name) {
-	var slides = document.getElementsByClassName(name);
-	var i;
-	for (i = 0; i < slides.length; i++)
-		if(slides[i].style.display == "block")
-			index = i;
-	
-	index += n;
-	showDivs(index, name);
-}
-
-function showDivs(index, name) {
-	var i;
-	var slides = document.getElementsByClassName(name);
-	var dots = document.getElementsByClassName(name + "Dots");
-
-	for (i = 0; i < slides.length; i++) {
-		if(slides[i].style.display == "block")
-			slides[i].style.display = "none";
+	for (let i = 0; i < slides.length; i++) {
+		const slide = slides[i];
+		if (i == slideIndex) {
+			var selectedSlide = slide;
+			slide.classList.add("selected");
+		}
+		else
+			slide.classList.remove("selected");
 	}
-
-	if (index >= slides.length) { index = 0; }
-	if (index < 0 ) { index = slides.length - 1; }
-
-	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" " + name + "Dots-selected", "");
+	const slideRootChildren = slideContainer.parentNode.parentNode.children;
+	for (let i = 0; i < slideRootChildren.length; i++) {
+		const child = slideRootChildren[i];
+		if (child.classList.contains("slides-bar-anchor")) {
+			var barElements = child.children[0].children;
+			for (let j = 0; j < barElements.length; j++) {
+				const element = barElements[j];
+				if (element.classList.contains("slide-caption"))
+					// supply caption
+					if(selectedSlide.alt !== "")
+					{
+						element.style.display = 'block';
+						element.children[0].innerHTML = selectedSlide.alt;
+					}
+					else
+					element.style.display = 'none';
+				else if (element.classList.contains("slide-dots")) {
+					for (let k = 0; k < element.children.length; k++) {
+						const dot = element.children[k];
+						if (k == slideIndex)
+							dot.classList.add("selected");
+						else {
+							console.debug(k + "!=" + slideIndex);
+							dot.classList.remove("selected");
+						}
+					}
+				}
+			}
+			return;
+		}
 	}
-	slides[index].style.display = "block";
-	dots[index].className += " " + name + "Dots-selected";
 }
 
-
-
-/*
-
-function carousel() {
-	plusDivs(+1);
-	setTimeout(carousel, carouselDelay * 1000);
+function getSelectedSlideIndex(targetId) {
+	var slideContainer = document.getElementById(targetId);
+	for (let i = 0; i < slideContainer.children.length; i++) {
+		const child = slideContainer.children[i];
+		if (child.classList.contains("selected")) {
+			return i;
+		}
+	}
+	return -1;
 }
 
-*/
+function moveSlide(targetId, slideIndexDelta) {
+	var slelectedIndex = getSelectedSlideIndex(targetId);
+	if (slelectedIndex == -1) {
+		console.error("Inavlid slide index");
+		return;
+	}
+	showSlide(targetId, slelectedIndex + slideIndexDelta);
+}
+
+// Display initial slides
+var slideContainers = document.getElementsByClassName("slides");
+for (const slideContainer of slideContainers) {
+	showSlide(slideContainer.id, 0);
+}
